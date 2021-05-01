7ccd6ff5cde3123140dd817987233ff79faff8c0
fabienkst <fabien.kst@gmail.com>
2021-05-01 13:41:04 +0200

    shake your booty

--- Alignement/blaster.py
+++ Alignement/blaster.py
@@ -5,7 +5,8 @@ from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 
 allhsp=[]
-final=[]
+final0=[]
+final1=[]
 
 maSeq = SeqIO.read('out_2.fasta',"fasta")
 blastSeq=maSeq
@@ -13,12 +14,18 @@ blastresult = NCBIWWW.qblast('blastn','nt',blastSeq.format('fasta'))
 	
 blast_record = NCBIXML.read(blastresult)
 list_hsp=list(blast_record.descriptions)
-final.append(list_hsp[0].title)
-final.append(list_hsp[0].score)
-final.append(list_hsp[0].evalue)
-print('hello')
+final0.append(list_hsp[0].title)
+#final.append(list_hsp[0].score)
+#final.append(list_hsp[0].evalue)
+
+final1.append(list_hsp[1].title)
+#final.append(list_hsp[1].score)
+#final.append(list_hsp[1].evalue)
 
 blastresult.close()
-print(final)
+print ('meilleur alignement (attention, il s_agit surement de la même séquence)')
+print(final0)
+print ('\n'+'deuxième meilleur alignement')
+print(final1)
 print ('done')
 
